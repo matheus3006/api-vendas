f9718aa0e8991f54f7054abf9020ceac24be1679
matheus3006 <matheussouza18@live.com>
2022-09-26 20:49:23 -0300

    feat: typeorm to import

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/App.Errors';
+import '@shared/typeorm';
 
 const app = express();
 app.use(cors());
