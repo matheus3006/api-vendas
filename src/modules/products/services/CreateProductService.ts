0381174a159bd4a8a7d7764d088dbcffc4bb08d6
matheus3006 <matheussouza18@live.com>
2022-09-29 08:03:47 -0300

    feat: add CreateProductService

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/App.Errors';
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductRepository';
+import Product from '../typeorm/entities/Product';
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository);
+    const productExists = await productRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('There is already a product with this name');
+    }
+
+    const product = productRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productRepository.save(product);
+    return product;
+  }
+}
+
+export default CreateProductService;
