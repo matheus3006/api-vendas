0d3d6a5b5f664710efbe7d99da8a63f82b35b4f0
matheus3006 <matheussouza18@live.com>
2022-09-29 19:56:54 -0300

    feat: add UpdateProductService

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductRepository';
+import Product from '../typeorm/entities/Product';
+import AppError from '@shared/errors/App.Errors';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class ShowProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository);
+    const product = await productRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    const productExists = await productRepository.findByName(name);
+
+    if (productExists && name !== product.name) {
+      throw new AppError('There is already a product with this name');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productRepository.save(product);
+    return product;
+  }
+}
+
+export default ShowProductService;
